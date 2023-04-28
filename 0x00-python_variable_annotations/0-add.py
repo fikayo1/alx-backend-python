111956dce44aa4ca6acf48080817929d2fdb77e3
Oluwafikayomi Soetan <soetandan@gmail.com>
2023-04-28 22:10:22 +0100

    Added readme

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+""" Type annotated function to add"""
+
+def add(a: int, b:int) -> float:
+    """ Function to return sum of a and B"""
+    return a+b
+
+print(add(2, 5.0))
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,1 @@
+# Type anotation in python
