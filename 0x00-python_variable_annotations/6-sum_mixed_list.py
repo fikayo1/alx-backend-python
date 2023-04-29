fef01e8272dc366a06e3afe47e5984a13b1eae95
Oluwafikayomi Soetan <soetandan@gmail.com>
2023-04-29 01:16:04 +0100

    python 3.7 error

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Mixed list."""
+from typing import Union, List
 
 
-def sum_mixed_list(mxd_lst: list[int | float]) -> float():
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float():
     """Return the sum of a list of numbers."""
     return sum(mxd_lst)
