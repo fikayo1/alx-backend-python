aa1e3195c2df41f7f7c476a54b1f532a3b850e6c
Oluwafikayomi Soetan <soetandan@gmail.com>
2023-05-03 08:57:37 +0100

    test file

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -7,14 +7,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> Coroutine:
-    """Returns a coroutine object that waits for a random amount of time.
-
-    Args:
-        max_delay (int): The maximum amount of time to wait, in seconds.
-
-    Returns:
-        Coroutine: A coroutine object that waits for a random amount of time.
-    """
-    # Create a task that will wait for a random amount of time
-    # using the `wait_random` coroutine function.
+    """Radmon wait"""
     return asyncio.create_task(wait_random(max_delay))
