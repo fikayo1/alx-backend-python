08635465e4fdcd6f769c0aa6c7502509ccb0253e
Oluwafikayomi Soetan <soetandan@gmail.com>
2023-05-03 08:58:51 +0100

    final

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -6,6 +6,6 @@ from typing import Coroutine
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Coroutine:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """Radmon wait"""
     return asyncio.create_task(wait_random(max_delay))
