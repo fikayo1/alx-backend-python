fa91fe58c2c6b03f3a43d3c8a1c3c94dbefa6043
Oluwafikayomi Soetan <soetandan@gmail.com>
2023-05-03 01:49:39 +0100

    completed task 4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Async function that calls another"""
+    tasks = [task_wait_random(max_delay) for i in range(n)]
+    val = await asyncio.gather(*tasks)
+    for i in range(len(val)):
+        for j in range(i + 1, len(val)):
+            if val[i] > val[j]:
+                val[i], val[j] = val[j], val[i]
+    return val
